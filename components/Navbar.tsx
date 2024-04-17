2d3c072315a4ebc9022087deea2b63cf4081111b
Xuan Lam <chim31102007@gmail.com>
2024-04-17 19:36:58 +0700

    Added Report Bug

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,11 @@
 import Image from "next/image";
 import React from "react";
 
+import { IoBugSharp } from "react-icons/io5";
+
 const Navbar: React.FC<{}> = () => {
   return (
-    <div className="w-full h-[65px] bg-['#111'] fixed backdrop-blur-sm z-50 px-10">
+    <div className="w-full h-[65px] bg-['#111'] fixed backdrop-blur-sm z-50 px-5 md:px-10">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
         <a href="/" className="h-auto w-auto flex flex-row items-center">
           <Image
@@ -20,10 +22,11 @@ const Navbar: React.FC<{}> = () => {
 
         <div className="flex flex-row gap-5">
           <div
-            onClick={() => window.open("/")}
-            className=" z-[1] bg-transparent  padding-10 cursor-pointer bg-black hover:bg-[#2E2E2E] rounded-xl  text-white  py-2 px-5"
+            title="Report a bug"
+            onClick={() => window.open("https://github.com/hoangxuanlam2007/hoangxuanlam2007/issues/new")}
+            className="transition-all duration-200 ease-linear z-[1] bg-transparent text-3xl padding-32 cursor-pointer bg-black hover:bg-[#ff4343] rounded-xl  text-white py-2 px-5"
           >
-            Bugs
+            <IoBugSharp />
           </div>
         </div>
       </div>
