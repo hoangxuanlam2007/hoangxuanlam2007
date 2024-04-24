15d5f868fea6a0d4d41db6112cbc402144f8f70b
Xuan Lam <chim31102007@gmail.com>
2024-04-24 11:02:58 +0700

    Fix border on Banner.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -36,10 +36,10 @@ const Experience: React.FC<{}> = () => {
             Next.js
           </div>
           <div className="bg-transparent  mt-5  mr-2 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            JavaScript
+            TypeScript
           </div>
           <div className="bg-transparent  mt-5 cursor-pointer  rounded-3xl  text-white py-2 px-5  border border-[#2E2E2E] w-max">
-            Typescript
+            JavaScript
           </div>
         </div>
       </div>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -71,56 +71,6 @@ const Projects: React.FC<{}> = () => {
           </Link>
 
           <Link
-            href="https://github.com/hoangxuanlam2007/ChimmLofiPlaylist"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="z-[1]"
-          >
-          <div className="rounded-2xl bg-[#1D232A]">
-            <div className="flex-row flex">
-              <div className="p-8 flex flex-col">
-                <div className="text-white opacity-60 font-normal text-lg flex flex-row content-center items-center flex-wrap gap-x-3"><IoMdLink />ChimmLofiPlaylist</div>
-                <div className="text-white opacity-60 font-thin text-sm mt-1 mb-5">
-                ChimmLofiPlaylist is a simple yet stylish music player built using React. The player offers basic functionalities like play, pause, skip, volume control, and even shuffle!
-                </div>
-                <div className="flex flex-row content-center justify-between items-center flex-wrap">
-                  <div className="flex flex-row content-center items-center flex-wrap gap-x-5">
-                    <div className="text-white opacity-60 font-thin text-sm flex flex-row content-center items-center flex-wrap gap-x-1" id="branches"><AiOutlineBranches />4</div>
-                    <div className="text-white opacity-60 font-thin text-sm flex flex-row content-center items-center flex-wrap gap-x-1" id="tags"><BsTag />4</div>
-                  </div>
-                  <div className="text-white opacity-60 font-thin text-sm flex flex-row content-center items-center flex-wrap gap-x-1"><FaCircle className="text-[#f1e05a]" /> JavaScript</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          </Link>
-
-          <Link
-            href="https://github.com/hoangxuanlam2007/ChimmPlaylist"
-            rel="noopener noreferrer"
-            target="_blank"
-            className="z-[1]"
-          >
-          <div className="rounded-2xl bg-[#1D232A]">
-            <div className="flex-row flex">
-              <div className="p-8 flex flex-col">
-                <div className="text-white opacity-60 font-normal text-lg flex flex-row content-center items-center flex-wrap gap-x-3"><IoMdLink />ChimmPlaylist</div>
-                <div className="text-white opacity-60 font-thin text-sm mt-1 mb-5">
-                  My very first project using pure HTML, CSS and JavaScript. This Web Music Player is very simple to use although the loading time is a bit slow as mp3 files are stored and loaded instantly onload.
-                </div>
-                <div className="flex flex-row content-center justify-between items-center flex-wrap">
-                  <div className="flex flex-row content-center items-center flex-wrap gap-x-5">
-                    <div className="text-white opacity-60 font-thin text-sm flex flex-row content-center items-center flex-wrap gap-x-1" id="branches"><AiOutlineBranches />1</div>
-                    <div className="text-white opacity-60 font-thin text-sm flex flex-row content-center items-center flex-wrap gap-x-1" id="tags"><BsTag />10</div>
-                  </div>
-                  <div className="text-white opacity-60 font-thin text-sm flex flex-row content-center items-center flex-wrap gap-x-1"><FaCircle className="text-[#E34C26]" /> HTML</div>
-                </div>
-              </div>
-            </div>
-          </div>
-          </Link>
-
-          <Link
             href="https://github.com/hoangxuanlam2007/BaloShulker"
             rel="noopener noreferrer"
             target="_blank"
@@ -164,6 +114,56 @@ const Projects: React.FC<{}> = () => {
                     <div className="text-white opacity-60 font-thin text-sm flex flex-row content-center items-center flex-wrap gap-x-1" id="tags"><BsTag />2</div>
                   </div>
                   <div className="text-white opacity-60 font-thin text-sm flex flex-row content-center items-center flex-wrap gap-x-1"><FaCircle className="text-[#A97BFF]" /> Kotlin</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          </Link>
+
+          <Link
+            href="https://github.com/hoangxuanlam2007/ChimmLofiPlaylist"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="z-[1]"
+          >
+          <div className="rounded-2xl bg-[#1D232A]">
+            <div className="flex-row flex">
+              <div className="p-8 flex flex-col">
+                <div className="text-white opacity-60 font-normal text-lg flex flex-row content-center items-center flex-wrap gap-x-3"><IoMdLink />ChimmLofiPlaylist</div>
+                <div className="text-white opacity-60 font-thin text-sm mt-1 mb-5">
+                ChimmLofiPlaylist is a simple yet stylish music player built using React. The player offers basic functionalities like play, pause, skip, volume control, and even shuffle!
+                </div>
+                <div className="flex flex-row content-center justify-between items-center flex-wrap">
+                  <div className="flex flex-row content-center items-center flex-wrap gap-x-5">
+                    <div className="text-white opacity-60 font-thin text-sm flex flex-row content-center items-center flex-wrap gap-x-1" id="branches"><AiOutlineBranches />4</div>
+                    <div className="text-white opacity-60 font-thin text-sm flex flex-row content-center items-center flex-wrap gap-x-1" id="tags"><BsTag />4</div>
+                  </div>
+                  <div className="text-white opacity-60 font-thin text-sm flex flex-row content-center items-center flex-wrap gap-x-1"><FaCircle className="text-[#f1e05a]" /> JavaScript</div>
+                </div>
+              </div>
+            </div>
+          </div>
+          </Link>
+
+          <Link
+            href="https://github.com/hoangxuanlam2007/ChimmPlaylist"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="z-[1]"
+          >
+          <div className="rounded-2xl bg-[#1D232A]">
+            <div className="flex-row flex">
+              <div className="p-8 flex flex-col">
+                <div className="text-white opacity-60 font-normal text-lg flex flex-row content-center items-center flex-wrap gap-x-3"><IoMdLink />ChimmPlaylist</div>
+                <div className="text-white opacity-60 font-thin text-sm mt-1 mb-5">
+                  My very first project using pure HTML, CSS and JavaScript. This Web Music Player is very simple to use although the loading time is a bit slow as mp3 files are stored and loaded instantly onload.
+                </div>
+                <div className="flex flex-row content-center justify-between items-center flex-wrap">
+                  <div className="flex flex-row content-center items-center flex-wrap gap-x-5">
+                    <div className="text-white opacity-60 font-thin text-sm flex flex-row content-center items-center flex-wrap gap-x-1" id="branches"><AiOutlineBranches />1</div>
+                    <div className="text-white opacity-60 font-thin text-sm flex flex-row content-center items-center flex-wrap gap-x-1" id="tags"><BsTag />10</div>
+                  </div>
+                  <div className="text-white opacity-60 font-thin text-sm flex flex-row content-center items-center flex-wrap gap-x-1"><FaCircle className="text-[#E34C26]" /> HTML</div>
                 </div>
               </div>
             </div>
